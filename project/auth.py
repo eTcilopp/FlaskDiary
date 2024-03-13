fa6e5ff083fdb834b12e75f4429233b0eef35201
eTcilopp <kirikeza@gmail.com>
2024-03-13 13:00:59 -0600

    authentication works

--- project/auth.py
+++ project/auth.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -52,5 +52,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('main.index'))
