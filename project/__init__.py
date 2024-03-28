e3e5dada9d3208101220292e96fb557f10c50e22
eTcilopp <kirikeza@gmail.com>
2024-03-28 15:55:55 -0600

    corrected error

--- project/__init__.py
+++ project/__init__.py
@@ -17,7 +17,8 @@ def create_app():
 
     app.config['SECRET_KEY'] = config["SECRET_KEY"]
     # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{config['MYSQL_USER']}:{confign['MYSQL_PASSWORD']}@db/{config['MYSQL_DATABASE']}"
+    app.config['SQLALCHEMY_DATABASE_URI'] =\
+        f"mysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@db/{config['MYSQL_DATABASE']}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     csrf = CSRFProtect(app)
