317e3ca457e1d3a90cd0c26ae402ea6db77655e6
eTcilopp <kirikeza@gmail.com>
2024-03-28 16:16:58 -0600

    minor update

--- project/__init__.py
+++ project/__init__.py
@@ -17,7 +17,7 @@ def create_app():
     app = Flask(__name__)
 
     app.config['SECRET_KEY'] = config["SECRET_KEY"]
-    print(os.environ.get('DOCKER_CONTAINER'))
+    print(os.environ.get('DOCKER_CONTAINER') == 1)
     if os.environ.get('DOCKER_CONTAINER') == 1:
         app.config['SQLALCHEMY_DATABASE_URI'] =\
             f"mysql://{config['MYSQL_USER']}:{config['MYSQL_PASSWORD']}@db/{config['MYSQL_DATABASE']}"
