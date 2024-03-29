610db8e34baa3003ebfab0816afe6931a6432039
eTcilopp <kirikeza@gmail.com>
2024-03-29 14:42:11 -0600

    works

--- project/__init__.py
+++ project/__init__.py
@@ -37,7 +37,8 @@ def create_app():
 
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        # return User.query.get(int(user_id))
+        return User.query.get(user_id)
 
     app.register_blueprint(auth_blueprint)
 
