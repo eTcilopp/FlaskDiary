30e11f58050f3512ce1b1079b28f94f3c9a31fa4
eTcilopp <kirikeza@gmail.com>
2024-04-16 20:29:57 -0600

    commented out migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,9 @@
 git pull
 
 # Migrate the database
-flask db init
-flask db migrate -m "Initial migration."
-flask db upgrade
+# flask db init
+# flask db migrate -m "Initial migration."
+# flask db upgrade
 
 # Start your application
 python3 app.py
