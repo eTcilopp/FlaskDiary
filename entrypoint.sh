0f5f00e5adc7f35bff8ad24c09eb665b4c5d0cea
eTcilopp <kirikeza@gmail.com>
2024-04-16 20:25:36 -0600

    added migrations

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Pull the latest changes from Git
+git pull
+
+# Migrate the database
+flask db init
+flask db migrate -m "Initial migration."
+flask db upgrade
+
+# Start your application
+python3 app.py
